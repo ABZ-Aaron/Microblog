46f50eb7940d78c1cf82bfabfed9927259e3fdbf
Aaron Wright <aaronjohnwright@gmail.com>
2021-08-29 21:05:05 +0100

    Updates to Database

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # The flask-wtf extension uses the secret key to protect against attacks
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-not-guess-this"
-    SQL_ALCHEMY_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
