b088a0a1c048e26e625001dbae820e448e45c367
Aaron Wright <aaronjohnwright@gmail.com>
2021-08-29 21:16:36 +0100

    Added Shell Context Processor

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from myapp import app
+from myapp import app, db
+from myapp.models import User, Post
+
+
+# When we invoke a Python shell using 'flask shell' these will be imported.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db' : db, 'User' : User, 'Post' : Post}
