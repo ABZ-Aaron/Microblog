5764b14564ce505cd38381580e37940c6199c26f
Aaron Wright <aaronjohnwright@gmail.com>
2021-08-29 20:38:51 +0100

    added database

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Here we are setting a secret key (this needs to exist for security purposes)
 class Config(object):
     # The flask-wtf extension uses the secret key to protect against attacks
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-not-guess-this"
+    SQL_ALCHEMY_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,10 @@
 # Import the Flask class
 from flask import Flask 
 
+# Used for our database
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 # Importing our class containing config vars
 from config import Config
 
@@ -8,8 +12,10 @@ from config import Config
 app = Flask(__name__)
 
 # Here we are reading and applying the config file, 
-# which contains are config variables.
+# which contains our config variables.
 app.config.from_object(Config)
+db = SQLAlchemy(app) # represents database
+migrate = Migrate(app, db) # represents migration database
 
-from myapp import routes
+from myapp import routes, models
 
