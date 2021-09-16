0a810aa2be710b30c532719ad8ba69fb9ef761b3
Aaron Wright <aaronjohnwright@gmail.com>
2021-09-16 08:49:47 +0100

    Added Error Email

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask # Import the Flask class
 from flask_sqlalchemy import SQLAlchemy # Used for our database
 from flask_migrate import Migrate # Used for our database
@@ -13,9 +15,25 @@ app = Flask(__name__)
 # Here we are reading and applying the config file, 
 # which contains our config variables.
 app.config.from_object(Config)
-
 db = SQLAlchemy(app) # represents database
 migrate = Migrate(app, db) # represents migration database
 login = LoginManager(app) # login instance of the LoginManager class
 login.login_view = 'login' # The view function that handles logins
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 from myapp import routes, models, errors
