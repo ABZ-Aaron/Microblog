776eb8da47f121767c7c619ad70b6705708e050f
Aaron Wright <aaronjohnwright@gmail.com>
2021-09-19 22:54:50 +0100

    Added in Unit Tests

--- myapp/models.py
+++ myapp/models.py
@@ -50,6 +50,16 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
 
 @login.user_loader
 def load_user(id):
--- myapp/test.py
+++
@@ -1,9 +0,0 @@
-y = [x for x in dir(set) if not x.startswith("__")]
-print(y)
-
-my_list = [1, 2, 3]
-
-
-
-
-list.
